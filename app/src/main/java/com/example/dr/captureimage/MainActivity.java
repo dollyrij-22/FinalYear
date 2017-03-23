82bbaa19032ba26cfac1092fb613f45bc5552595
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-23 13:20:36 -0700

    final changes

--- app/src/main/java/com/example/dr/captureimage/MainActivity.java
+++ app/src/main/java/com/example/dr/captureimage/MainActivity.java
@@ -1,32 +1,42 @@
 package com.example.dr.captureimage;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String UPLOAD_URL = "http://attendance-dr22libraryapp.rhcloud.com/file_upload.php";
+    public static final String UPLOAD_KEY = "image";
+    public static final String TAG = "MY MESSAGE";
     private ImageView imageHolder;
     private final int requestCode = 20;
+    Button buttonUpload,capturedImageButton;
+    private int PICK_IMAGE_REQUEST = 1;
+    private Bitmap bitmap;
+    String encodedImage;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageHolder = (ImageView)findViewById(R.id.captured_photo);
-        Button capturedImageButton = (Button)findViewById(R.id.photo_button);
-        capturedImageButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(photoCaptureIntent, requestCode);
-            }
-        });
+        capturedImageButton = (Button)findViewById(R.id.photo_button);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -34,6 +44,60 @@ public class MainActivity extends AppCompatActivity {
         if(this.requestCode == requestCode && resultCode == RESULT_OK){
             Bitmap bitmap = (Bitmap)data.getExtras().get("data");
             imageHolder.setImageBitmap(bitmap);
+        }
+    }
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+        class UploadImage extends AsyncTask<Bitmap,Void,String>{
+
+            ProgressDialog loading;
+            RequestHandler rh = new RequestHandler();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this, "Uploading Image", "Please wait...",true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Bitmap... params) {
+                Bitmap bitmap = params[0];
+                String uploadImage = getStringImage(bitmap);
+
+                HashMap<String,String> data = new HashMap<>();
+                data.put(UPLOAD_KEY, uploadImage);
+
+                String result = rh.sendPostRequest(UPLOAD_URL,data);
+
+                return result;
+            }
+        }
+
+        UploadImage ui = new UploadImage();
+        ui.execute(bitmap);
+        }
+    @Override
+    public void onClick(View v) {
+        if (v == capturedImageButton) {
+            Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(photoCaptureIntent, requestCode);
+        }
+        if(v == buttonUpload) {
+            uploadImage();
         }
     }
 }

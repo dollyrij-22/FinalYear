7b5261f40e9bb0f59a3b89109e3efec1d322f308
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-29 13:33:52 -0700

    login

--- app/src/main/java/com/example/dr/finalyear/Home.java
+++ app/src/main/java/com/example/dr/finalyear/Home.java
@@ -1,23 +1,52 @@
 package com.example.dr.finalyear;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by DR on 3/29/2017.
  */
 public class Home extends Fragment {
+    String username;
     public Home() {
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home, container, false);
+        Button attn = (Button) view.findViewById(R.id.attendence);
+        Button image = (Button) view.findViewById(R.id.image);
+        username=getActivity().getIntent().getStringExtra("username");
+        if(username!=null)
+        {
+            attn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    intent.putExtra("username",username);
+                    getActivity().startActivity(intent);
+                }
+            });
+            image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity().getApplicationContext(), "yet not implemented!!!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+        else
+        {
+            getActivity().finish();
+            Toast.makeText(getActivity().getApplicationContext(), "Incorrect Username or password !!!\n or Check your Internet connection", Toast.LENGTH_LONG).show();
+        }
         return view;
     }
+
 }

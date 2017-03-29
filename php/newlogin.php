102a526f2c9ddff3c88d8da4ce91bb874acb1bc0
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-29 10:41:10 -0700

    login

--- php/newlogin.php
+++ php/newlogin.php
@@ -6,7 +6,7 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 
-$sql = "SELECT * FROM 'login' WHERE 'username'="$username" AND 'password'="$password" ";
+$sql = "SELECT * FROM login WHERE username= '$username' AND password = '$password' ";
 
 $result = mysqli_query($con, $sql);
 

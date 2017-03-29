a0cd8b755825adef1f15bdd624c2930819311e76
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-29 11:33:17 -0700

    image

--- php/image_upload.php
+++ php/image_upload.php
@@ -20,7 +20,7 @@ require "init.php";
 		
 		$actualpath = "http://attendance-dr22libraryapp.rhcloud.com/$path";
 		
-		$sqlm = "INSERT INTO image (image,username,`date`,`time`) VALUES ('$actualpath','$username',CURDATE(), "00:00:00")";
+		$sqlm = "INSERT INTO image (image,username,`date`,`time`) VALUES ('$actualpath','$username',CURDATE(), LOCALTIME())";
 		
 		if(mysqli_query($con,$sqlm)){
 			file_put_contents($path,base64_decode($image));

06f8c0f7b459fc229014dd1667a02708e3b6a7a6
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-29 10:47:28 -0700

    login

--- php/newlogin.php
+++ php/newlogin.php
@@ -15,10 +15,11 @@ $sql = "SELECT * FROM `login` WHERE `username` = '".$username."' AND `password` 
 
 $result = mysqli_query($con, $sql)or die(mysql_error());
 
-$response = array();
-
-while($row = mysqli_fetch_array($result)){
-	$response = array("name"=>$row[0],"designation"=>$row[1],"specialization"=>$row[2],"phone"=>$row[3],"email"=>$row[4],"department"=>$row[5],"username"=>$row[6],"password"=>$row[7]);
+$row = mysqli_fetch_array($result);
+if(isset($row)){
+echo 'success';
+}else{
+echo 'failure';
 }
-echo json_encode(array("user_data"=>$response));
+mysqli_close($con);
 ?>

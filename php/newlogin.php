9c09829bb4c8be273add33ffadae2021a3298ee7
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-29 10:44:57 -0700

    login

--- php/newlogin.php
+++ php/newlogin.php
@@ -6,13 +6,13 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 
-$sql = "SELECT * FROM login WHERE username= '$username' AND password = '$password' ";
+$sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password' ";
 
-$result = mysqli_query($con, $sql);
+$result = mysql_query($con, $sql);
 
 $response = array();
 
-while($row = mysqli_fetch_array($result)){
+while($row = mysql_fetch_array($result)){
 	$response = array("designation"=>$row[1],"name"=>$row[0],"password"=>$row[7],"email"=>$row[4],"department"=>$row[5],"specialization"=>$row[2],"phone"=>$row[3],"username"=>$row[6]);
 }
 echo json_encode(array("user_data"=>$response));

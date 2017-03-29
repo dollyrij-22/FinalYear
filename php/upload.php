596967f7df1025bad195f6f4e1de90173646db20
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-29 12:55:26 -0700

    php

--- php/upload.php
+++ php/upload.php
@@ -2,10 +2,7 @@
 error_reporting(0);
 require "init.php";
 
-	if($_SERVER['REQUEST_METHOD']=='POST'){	
-        date_default_timezone_set('Asia/Calcutta');	
-		$temp= strtotime("+5 hours 30 minutes"); 
-        $time = time("H:i:s",$temp);
+	if($_SERVER['REQUEST_METHOD']=='POST'){		
 		
 		$image = $_POST['image'];
 		
@@ -24,7 +21,7 @@ require "init.php";
 		
 		$actualpath = "http://attendance-dr22libraryapp.rhcloud.com/$path";
 		
-		$sqlm = "INSERT INTO image (image,username,`date`,`time`) VALUES ('$actualpath','$username',CURDATE(),'$time')";
+		$sqlm = "INSERT INTO image (image,username,`date`,`time`) VALUES ('$actualpath','$username',CURDATE(),CURTIME())";
 		
 		if(mysqli_query($con,$sqlm)){
 			file_put_contents($path,base64_decode($image));

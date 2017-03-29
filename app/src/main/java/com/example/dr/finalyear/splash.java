8d3e4b92dfa00a5601e12edb83dd28bd1aaefdaa
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-28 22:29:21 -0700

    login

---
+++ app/src/main/java/com/example/dr/finalyear/splash.java
@@ -0,0 +1,35 @@
+package com.example.dr.finalyear;
+
+/**
+ * Created by DR on 3/28/2017.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DR on 1/2/2017.
+ */
+public class splash extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread splashThread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+                    Intent startMainScreen = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(startMainScreen);
+                    finish();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        splashThread.start();
+    }
+}
---
+++ php/newlogin.php
@@ -0,0 +1,23 @@
+<?php 
+error_reporting(0);
+require "init.php";
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+//$name = "sdf";
+//$password = "sdf";
+
+$sql = "SELECT * FROM `login` WHERE `username`='".$username."' AND `password`='".$password."';";
+
+$result = mysqli_query($con, $sql);
+
+$response = array();
+
+while($row = mysqli_fetch_array($result)){
+	$response = array("name"=>$row[0],"designation"=>$row[1],"specialization"=>$row[2],"phone"=>$row[3],"email"=>$row[4],"department"=>$row[5],"password"=>$row[7],"username"=>$row[6]);
+}
+
+echo json_encode(array("user_data"=>$response));
+
+?>
